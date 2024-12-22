22dd0a91d8a44f63a8ab3f8305f343e11ef9e1c6
Dylan <changliangdylan@gmail.com>
2024-12-22 12:40:17 -0500

    try again xd

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,4 +3,8 @@ const router = require("express").Router();
 // Video Download Route
 router.use("/video-download", require("./video/video-download.route"))
 
+router.get("/", (req, res) => {
+    res.status(200).send("Successful")
+})
+
 module.exports = router;
